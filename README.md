9361e46c9b350de086170a35055d52633110aee7
thapa10sujan <thapa10sujan@gmail.com>
2023-07-24 08:11:03 +0545

    added register page

--- README.md
+++ README.md
@@ -2,28 +2,47 @@
 ## Roadmap
    
    TRAVERSE NEPAL
+    - online Booking website with travel information 
+
+
 
 ## About The Project
+
+Traverse Nepal is a travel tourism website where users can book and travel to different places in Nepal. The website provides information on various tourist destinations in Nepal, including popular attractions, accommodations, and travel options. 
+
+
+## Features
+
+- Information on popular tourist destinations in Nepal
+- Accommodation booking
+- Travel booking
+- Reviews and ratings from other users
+- Interactive maps of tourist destinations
+- Local events and festivals calendar
+
+
+## Installation
+
+To run this project, you need to have the following software installed on your computer:
+
+- Node.js
+- MongoDB
   
- PHASE I
-
-- [] Login
-     - [] Admin
-     - [] User
-
-
-- [] Register
-    - [] Admin
-    - [] User
-
 
   
-### Built With
+### Technologies Used
 
+- HTML
+- CSS
+- JavaScript
+- Bootstrap
+- Node.js
+- Express.js
+- MongoDB
 * Next.js
 * React.js
 
 
 
-## Getting Started
+
 
--- server/src/index.js
+++ server/src/index.js
@@ -7,7 +7,6 @@ const app = express()
 const port = process.env.port
 app.use(express.json())
 
-
 app.post('/register', (req, res) => {
  Users.create(req.body)
  res.json({
@@ -15,21 +14,21 @@ app.post('/register', (req, res) => {
  })
 })
  
-app.get('/products', async(req, res) => {
- const data = await Products.find()
-})
+// app.get('/products', async(req, res) => {
+//  const data = await Products.find()
+// })
  
-app.put('/products/:id', async(req, res) => {
- await Products.findByIdAndUpdate(req.params.id, req.body)
- })
+// app.put('/products/:id', async(req, res) => {
+//  await Products.findByIdAndUpdate(req.params.id, req.body)
+//  })
  
- app.delete('/products/:id', async(req, res) => {
- await Products.findByIdAndDelete(req.params.id)
- })
+//  app.delete('/products/:id', async(req, res) => {
+//  await Products.findByIdAndDelete(req.params.id)
+//  })
  
- app.get('/products', async(req, res) => {
- const data = await Products.find()
- })
+//  app.get('/products', async(req, res) => {
+//  const data = await Products.find()
+//  })
 
 
  app.listen(port, () => {
