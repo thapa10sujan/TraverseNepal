ce3ef3d98a1d07fcb1f1d2c51cdbd89736599b0d
thapa10sujan <thapa10sujan@gmail.com>
2023-08-02 11:34:36 +0545

    redux persist

--- client/src/pages/_app.js
+++ client/src/pages/_app.js
@@ -1,10 +1,13 @@
 import '@/styles/globals.css'
 import { Provider } from 'react-redux'
-import store from '../redux/store/index'
+import { PersistGate } from 'redux-persist/integration/react';
+import { persistor, store } from '../redux/store/index';
 export default function App({ Component, pageProps }) {
   return(
     <Provider store={store}>
+    <PersistGate loading={null} persistor={persistor}>
     <Component {...pageProps} />
+    </PersistGate>
     </Provider>
     )
 }
--- client/src/redux/store/index.js
+++ client/src/redux/store/index.js
@@ -2,16 +2,29 @@
 import { configureStore } from '@reduxjs/toolkit'
 import { combineReducers } from 'redux'
 import logger from 'redux-logger'
-
+import storage from 'redux-persist/lib/storage';
+import { persistReducer, persistStore } from 'redux-persist';
 import users from '../reducerSlice/user'
+
+
+const persistConfig = {
+  key: 'root',
+  storage,
+}
+
+
 const reducer = combineReducers({
     users,
   })
 
+  const persistedReducer = persistReducer(persistConfig, reducer)
 
-  const store = configureStore({
-    reducer,
+
+   export const store = configureStore({
+    reducer: persistedReducer,
     middleware:[logger]
   })
+
+  export const persistor = persistStore(store)
+    
   
-  export default store;
